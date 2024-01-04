109ff5628a778199d7b0bc9448413d91ea931f9e
Johan Swan <jswannieman@gmail.com>
2024-01-05 10:18:57 +1300

    Update AddTransaction JSX Component

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -20,7 +20,7 @@ export const AddTransaction = () => {
   };
 
   return (
-    <>
+    <div>
       <h3>Add new transaction</h3>
       <form onSubmit={onSubmit}>
         <div className="form-control">
@@ -34,8 +34,7 @@ export const AddTransaction = () => {
         </div>
         <div className="form-control">
           <label htmlFor="amount">
-            Amount <br />
-            (negative - expense, positive - income)
+            Amount (negative - expense, positive - income)
           </label>
           <input
             type="number"
@@ -46,6 +45,6 @@ export const AddTransaction = () => {
         </div>
         <button className="btn">Add transaction</button>
       </form>
-    </>
+    </div>
   );
 };
