2c920581318dd7ff86954d0c2df827feb437d3db
Johan Swan <jswannieman@gmail.com>
2024-01-05 10:19:32 +1300

    Update TransactionList JSX Component

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -7,13 +7,13 @@ export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
 
   return (
-    <>
+    <div>
       <h3>History</h3>
       <ul className="list">
         {transactions.map((transaction) => (
           <Transaction key={transaction.id} transaction={transaction} />
         ))}
       </ul>
-    </>
+    </div>
   );
 };
