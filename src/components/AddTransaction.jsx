6a651444f6ef85385046491902bcdb81e19dafbb
Johan Swan <jswannieman@gmail.com>
2024-01-05 10:36:04 +1300

    Update AddTransaction JSX Component

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -24,7 +24,6 @@ export const AddTransaction = () => {
       <h3>Add new transaction</h3>
       <form onSubmit={onSubmit}>
         <div className="form-control">
-          <label htmlFor="text">Text</label>
           <input
             type="text"
             value={text}
